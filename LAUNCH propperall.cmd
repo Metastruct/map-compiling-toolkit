701df6473c73459011d562f233a420b041c6e619
Python1320 <python1320@users.noreply.github.com>
2016-09-04 00:14:35 +0300

    WIP propper support

---
+++ LAUNCH propperall.cmd
@@ -0,0 +1,50 @@
+@rem Store current folder
+@set CMD_LC_ROOT=%~dp0
+
+@cd /d "%CMD_LC_ROOT%"
+@call config.bat
+@cd /d "%CMD_LC_ROOT%"
+@title Propper Batch Compiler
+
+@set QC_STORAGE=%mapfolder%\propper
+
+@set VBSPNAME=vbsp_propper
+@%VBSPNAME% >nul 2>nul
+@if ERRORLEVEL 9009 goto nofound
+@goto found
+
+
+:nofound
+@echo ERROR: %VBSPNAME% could not be found.
+@pause > nul
+@exit 1
+
+:found
+@cd /d "%mapfolder%\propper"
+@echo.
+@for /r %%i in (*.vmf) do %VBSPNAME% %%i > "%%i.log"
+@if ERRORLEVEL 1 goto failed
+@goto win
+
+
+
+:failed
+@echo COMPILE FAILURE!
+
+@pause > nul
+@goto gtfo
+
+
+
+
+
+:win
+@echo =======================
+@echo ====== FINISHED =======
+@echo =======================
+
+@goto gtfo
+
+
+:gtfo
+@echo.
