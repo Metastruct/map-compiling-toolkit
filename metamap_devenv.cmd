d8f6be7cb02cf280cac6f2f170ed93916a42289f
Python1320 <python1320@users.noreply.github.com>
2020-03-04 18:53:43 +0200

    Improve devenv setup script

--- metamap_devenv.cmd
+++ metamap_devenv.cmd
@@ -2,11 +2,11 @@
 
 @echo # Test if git exists
 git --version
-IF ERRORLEVEL 1 GOTO fail
+@IF ERRORLEVEL 1 GOTO fail
 
 @echo # Test if gitlab is accessible
 git ls-remote git@gitlab.com:metastruct/mapfiles.git
-IF ERRORLEVEL 1 GOTO fail
+@IF ERRORLEVEL 1 GOTO fail
 
 
 @echo # Clone all required repos
@@ -18,20 +18,27 @@ git clone git@gitlab.com:metastruct/mapfiles.git mapfiles
 @echo # Autosetup folders and paths
 
 cd map-compiling-toolkit\setup
+setup.py
+@IF ERRORLEVEL 1 GOTO setup_py_not_ok
+@goto setup_py_ok
+
+:setup_py_not_ok
 setup.exe
-IF ERRORLEVEL 1 GOTO fail
+@IF ERRORLEVEL 1 GOTO fail
+:setup_py_ok
+
 cd ..
 
 @echo # run propper once to build models
 "LAUNCH propperall.cmd"
-IF ERRORLEVEL 1 GOTO fail
+@IF ERRORLEVEL 1 GOTO fail
 
 notepad user_config.cmd
 
-echo DEVENV AUTOSETUP FINISHED. You're on your own.
-goto end
+@echo DEVENV AUTOSETUP FINISHED. You're on your own.
+@goto end
 :fail 
-echo FAILURE
+@echo FAILURE
 
 :end
-pause
+@pause
