28e487f51ca5692e0a457fdd811e0be80389725e
Python1320 <python1320@users.noreply.github.com>
2020-03-04 18:32:46 +0200

    Add more error checking

--- metamap_devenv.cmd
+++ metamap_devenv.cmd
@@ -1,19 +1,31 @@
 @prompt $G$S
 
+@echo # Test if git exists
 git --version
 IF ERRORLEVEL 1 GOTO fail
 
-git clone https://github.com/Metastruct/map-compiling-toolkit.git
+@echo # Test if gitlab is accessible
+git ls-remote git@gitlab.com:metastruct/mapfiles.git
+IF ERRORLEVEL 1 GOTO fail
+
+
+@echo # Clone all required repos
+git clone https://github.com/Metastruct/map-compiling-toolkit.git map-compiling-toolkit
 git clone git@gitlab.com:metastruct/mapdata.git mapdata
 git clone https://github.com/Metastruct/meta-mapassets mapdata2
 git clone git@gitlab.com:metastruct/mapfiles.git mapfiles
 
+@echo # Autosetup folders and paths
+
 cd map-compiling-toolkit\setup
 setup.exe
-
-
+IF ERRORLEVEL 1 GOTO fail
 cd ..
+
+@echo # run propper once to build models
 "LAUNCH propperall.cmd"
+IF ERRORLEVEL 1 GOTO fail
+
 notepad user_config.cmd
 
 echo DEVENV AUTOSETUP FINISHED. You're on your own.
